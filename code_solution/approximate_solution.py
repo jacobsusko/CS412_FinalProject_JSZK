e8b93a9266f99c0579b8d6fdfccb4f29f8b98276
Zach Kiser <115175671+Zach-Kiser@users.noreply.github.com>
2024-04-23 11:32:01 -0400

    4/23

---
+++ code_solution/approximate_solution.py
@@ -0,0 +1,44 @@
+def main():
+    vertex_count, edge_count = [int(x) for x in input().split()]
+
+    adj_list = {}
+    start = None
+    for i in range(edge_count):
+        u, v, w = [x for x in input().split()]
+
+        if not start:
+            start = u
+
+        if u not in adj_list:
+            adj_list[u] = {v: int(w)}
+        else:
+            adj_list[u][v] = int(w)
+
+    total, path = greedy_search(adj_list, start)
+    print(total)
+    for i in path:
+        print(i, end=" ")
+    print()
+
+
+def greedy_search(adj_list, start):
+    visited = set()
+    path = [start]
+    total = 0
+    while len(visited) <= len(adj_list.keys()):
+        if start not in adj_list:
+            break
+        max = -1
+        max_vertex = None
+        for j in adj_list[start]:
+            if adj_list[start][j] > max:
+                max = adj_list[start][j]
+                max_vertex = j
+        visited.add(max_vertex)
+        path.append(max_vertex)
+        total += max
+        start = j
+    return total, path
+
+if __name__ == "__main__":
+    main()
