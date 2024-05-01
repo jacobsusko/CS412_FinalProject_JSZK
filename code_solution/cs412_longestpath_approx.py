298ea479edc33cd1e640f2bc6f771a7d541e4609
Zach Kiser <115175671+Zach-Kiser@users.noreply.github.com>
2024-05-01 10:24:48 -0400

    Fix inf loop

--- code_solution/cs412_longestpath_approx.py
+++ code_solution/cs412_longestpath_approx.py
@@ -17,7 +17,9 @@ def main():
     total, path = greedy_search(adj_list, start)
     print(total)
     for i in path:
-        print(i, end=" ")
+        if i != path[0]:
+            print(end=" ")
+        print(i, end="")
     print()
 
 
@@ -25,19 +27,22 @@ def greedy_search(adj_list, start):
     visited = set()
     path = [start]
     total = 0
-    while len(visited) <= len(adj_list.keys()):
+
+    while len(visited) < len(adj_list) - 1:
         if start not in adj_list:
             break
         max = -1
         max_vertex = None
         for j in adj_list[start]:
-            if adj_list[start][j] > max:
+            if adj_list[start][j] > max and j not in visited:
                 max = adj_list[start][j]
                 max_vertex = j
+        if not max_vertex:
+            break
         visited.add(max_vertex)
         path.append(max_vertex)
         total += max
-        start = j
+        start = max_vertex
     return total, path
 
 if __name__ == "__main__":
